edcd2acfdd82ea5ebb1e3466d1e56f73988e9fcb
MikroMirza <mirkodjukic314@gmail.com>
2025-08-02 23:25:35 +0200

    [FIX] quick fix

--- MA/app/build.gradle.kts
+++ MA/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("io.reactivex.rxjava2:rxjava:2.2.21")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.15.1")
     kapt("com.github.bumptech.glide:compiler:4.15.1")
