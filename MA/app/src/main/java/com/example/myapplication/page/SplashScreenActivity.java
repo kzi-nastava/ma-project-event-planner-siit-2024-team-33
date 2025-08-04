f102c81b70789633c12bf707190ccb48d16a6c0e
MikroMirza <mirkodjukic314@gmail.com>
2025-08-04 13:45:49 +0200

    [FIX] made the user go to mainpage first

--- MA/app/src/main/java/com/example/myapplication/page/SplashScreenActivity.java
+++ MA/app/src/main/java/com/example/myapplication/page/SplashScreenActivity.java
@@ -1,31 +1,24 @@
 package com.example.myapplication.page;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static final int SPLASH_DELAY = 5000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_splash_screen);
 
-
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+            Intent intent = new Intent(SplashScreenActivity.this, MainPageActivity.class);
             startActivity(intent);
             finish();
-        }, SPLASH_DELAY);
+        }, 1500);
     }
-
-
 }
